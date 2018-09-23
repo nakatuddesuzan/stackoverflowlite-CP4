08e96b87f9291205b04d89316a090a1b3e484496
nakatuddesuzan <nakatuddesusan@gmail.com>
2018-09-23 16:11:34 +0300

    Fetch delete question endpoint

--- Javascript/delete_question.js
+++ Javascript/delete_question.js
@@ -5,4 +5,15 @@ function deleteQuestion(){
     if (localStorage.getItem('token')){
         let url = "http://127.0.0.1:5000/api/v1/question/" + qtn_id;
     }
+
+    fetchAPI(url,'delete',newQuestion)
+        .then(results => {
+
+            if (results.status === 201) {
+                    alert("Your question has been deleted")
+                }
+        }
+    ).catch(function(error){
+        console.log(error)
+    })
 }
