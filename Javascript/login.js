90f55334d324b1c393782269cab51e518c7bdfe3
nakatuddesuzan <nakatuddesusan@gmail.com>
2018-09-26 18:58:38 +0300

    Fix heroku app

--- Javascript/login.js
+++ Javascript/login.js
@@ -22,7 +22,7 @@ function signinUser(event) {
             if(!results) return;
             if (results.status === 200 && results.data.token) {
             localStorage.setItem('token', results.data.token);
-            localStorage.setItem("name", display_name);
+            localStorage.setItem("username", username);
             console.log(localStorage.getItem('token'))
             window.location.href = "profile.html"
         }
