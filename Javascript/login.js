fba03718960a62818b3c5edf7fb2439493782fa3
nakatuddesuzan <nakatuddesusan@gmail.com>
2018-09-26 10:59:46 +0300

    fix app

--- Javascript/login.js
+++ Javascript/login.js
@@ -22,6 +22,7 @@ function signinUser(event) {
             if(!results) return;
             if (results.status === 200 && results.data.token) {
             localStorage.setItem('token', results.data.token);
+            localStorage.setItem("name", display_name);
             console.log(localStorage.getItem('token'))
             window.location.href = "../html/profile.html"
         }
